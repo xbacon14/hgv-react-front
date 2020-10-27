c6fa67f2df6f334de6c4b775d9ec8b6302071c19
Italo Golin <47801578+xbacon14@users.noreply.github.com>
2020-10-27 19:29:00 -0300

    Update index.jsx

--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -7,127 +7,127 @@ import '../../components/alerts/alertCiNotFound.jsx';
 
 const Home = () => {
 
-    // const [timeOut, setTimeout] = useState(0)
+  // const [timeOut, setTimeout] = useState(0)
 
-    const [data, setData] = useState([]);
-    const [cic, setCi] = useState();
-    const [show, setShow] = useState(true);
-
-
-    const loadRegistros = async () => {
-        var lista = [];
-        var count = 1;
-
-        await db.collection('registros').orderBy("createdAt", "desc").limit(4).get().then(
-            (snapshot) => {
-                // console.log(snapshot)
-                snapshot.docs.forEach(
-                    doc => {
-                        var datos = doc.data();
-                        var objeto = datos.linkObject;
-                        var fecha = datos.createdAt.toDate();
-                        var fechitalo = new Intl.DateTimeFormat('es-PY', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' }).format(fecha);
-                        lista.push({ ...objeto, count, fechitalo });
-                        // console.log(lista);
-                        count++;
-
-                    }
-                );
-                setData(lista)
-                console.log(lista);
-                console.log(count);
+  const [data, setData] = useState([]);
+  const [cic, setCi] = useState();
+  const [show, setShow] = useState(true);
 
 
-            }
-        ).catch(err => <Link to="/error404" />)
-    }
+  const loadRegistros = async () => {
+    var lista = [];
+    var count = 1;
 
-    const consultarCi = async () => {
-        var ci = cic;
-        if (ci.length > 5 & ci.length < 8) {
-            await db.collection('personas').where("ci", "==", ci).get().then(
-                (querySnapshot) => {
-                    if (querySnapshot.empty) {
-                        AlertCiNotFound()
-                    } else {
-                        querySnapshot.forEach((doc) => {
-                            console.log(doc.id, "=>", doc.data())
-                        })
-                    }
-                }
+    await db.collection('registros').orderBy("createdAt", "desc").limit(4).get().then(
+      (snapshot) => {
+        // console.log(snapshot)
+        snapshot.docs.forEach(
+          doc => {
+            var datos = doc.data();
+            var objeto = datos.linkObject;
+            var fecha = datos.createdAt.toDate();
+            var fechitalo = new Intl.DateTimeFormat('es-PY', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' }).format(fecha);
+            lista.push({ ...objeto, count, fechitalo });
+            // console.log(lista);
+            count++;
 
-            )
-        } else {
-            return (<Alert> El campo CI deve tener al menos 6 carácteres</Alert>)
+          }
+        );
+        setData(lista)
+        console.log(lista);
+        console.log(count);
+
+
+      }
+    ).catch(err => <Link to="/error404" />)
+  }
+
+  const consultarCi = async () => {
+    var ci = cic;
+    if (ci.length > 5 & ci.length < 8) {
+      await db.collection('personas').where("ci", "==", ci).get().then(
+        (querySnapshot) => {
+          if (querySnapshot.empty) {
+            AlertCiNotFound()
+          } else {
+            querySnapshot.forEach((doc) => {
+              console.log(doc.id, "=>", doc.data())
+            })
+          }
         }
+
+      )
+    } else {
+      return (<Alert> El campo CI deve tener al menos 6 carácteres</Alert>)
     }
+  }
 
-    const AlertCiNotFound = () => {
+  const AlertCiNotFound = () => {
 
 
-        if (show) {
-            console.log("Esta intentando alertar")
-            return (<>
-                <Alert variant="danger" show={show} onClose={() => setShow(false)} dismissible>
-                    <Alert.Heading>
-                        C.I. no encontrada
+    if (show) {
+      console.log("Esta intentando alertar")
+      return (<>
+        <Alert variant="danger" show={show} onClose={() => setShow(false)} dismissible>
+          <Alert.Heading>
+            C.I. no encontrada
                     </Alert.Heading>
-                </Alert>
-            </>
-            )
-        } else {
-            return <button onclick={() => setShow(true)}>Show Alert modafoca</button>;
+        </Alert>
+      </>
+      )
+    } else {
+      return <button onclick={() => setShow(true)}>Show Alert modafoca</button>;
 
-        }
     }
+  }
 
 
-    const handleInput = (e) => {
-        var ci = e.target.value;
-        setCi(ci)
-    }
+  const handleInput = (e) => {
+    var ci = e.target.value;
+    setCi(ci)
+  }
 
-    useEffect(() => {
-        loadRegistros();
+  useEffect(() => {
+    loadRegistros();
 
-    }, [])
+  }, [])
 
-    return (
-        <div >
-            <div className="registro-container col-6">
-                <label htmlFor="ci" >CONSULTE POR CI *</label>
-                <div className="row d-flex " id="row-home">
-                    <input id="ci-home"
-                        required
-                        className="col-6"
-                        type="number"
-                        maxLength='7'
-                        name="ci"
-                        // autoComplete="off"
-                        onChange={handleInput}
-                        placeholder="Ingrese su ci" />
-                    <button className="btn btn-success" onClick={consultarCi} id="consultar"> Registrar </button>
-                </div>
-            </div>
+  return (
+    <div >
+      <div className="registro-container col-6">
+        <label htmlFor="ci" >CONSULTE POR CI *</label>
+        <div className="row d-flex " id="row-home">
+          <input id="ci-home"
+            required
+            className="col-6"
+            type="number"
+            maxLength='7'
+            name="ci"
+            // autoComplete="off"
+            onChange={handleInput}
+            placeholder="Ingrese su ci" />
+          <button className="btn btn-success" onClick={consultarCi} id="consultar"> Registrar </button>
+        </div>
+      </div>
 
 
-            <div className="ult-registros col-4">
-                {
-                    data.map((data) => {
-                        return (
-                            <div key={data.count} className="card" >
-                                <div className="card-body" >
-                                    <span className="card-title" > {data.nombre} </span>
-                                    <p className="card-text" >
-                                        {data.fechitalo} Hs.
+      <div className="ult-registros col-4">
+        {
+          data.map((data) => {
+            return (
+              <div key={data.count} className="card" >
+                <div className="card-body" >
+                  <span className="card-title" > {data.nombre} </span>
+                  <p className="card-text" >
+                    {data.fechitalo} Hs.
                                                 </p>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-            {/* <div className="row cuadrados">
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+      {/* <div className="row cuadrados">
                 <div className="cards col-6" id="textos-home">
                     <div className="card-title" id="title-home">AYUDEMOS A FRENAR EL CORONGA.</div>
                     <p id="text-home">Registrando tu entrada en algún establecimiento aherido
@@ -141,8 +141,8 @@ que todos estemos prevenidos.</p>
                     <button className="btn btn-large" id="empecemos">EMPEZAR</button>
                 </div>
             </div> */}
-        </div >
-    )
+    </div >
+  )
 }
 
 export default Home;
